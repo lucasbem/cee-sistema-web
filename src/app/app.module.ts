d758738b19d3719362c91f0ceaa11e18e0208ded
haroldocruz <msom.info@gmail.com>
2020-09-06 02:22:32 -0300

    Create Home feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './layout/layout.component';
-import { TopComponent } from './layout/top/top.component';
-import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
