b71448abf5c7b52cb02b15672427f18e6f80a724
Haroldo Cruz <msom.info@gmail.com>
2020-09-09 14:49:48 -0300

    refactor(app): module layout; OverlayScrollbars
    Create Layout module to encapsulate layout`s components; add OverlayScrollbars in Nav component.

---
+++ src/app/layout/nav/nav-perfil/nav-perfil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-perfil',
+  templateUrl: './nav-perfil.component.html',
+  styleUrls: ['./nav-perfil.component.less']
+})
+export class NavPerfilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
