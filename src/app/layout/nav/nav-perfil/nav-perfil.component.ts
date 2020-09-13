e77cc9ef27c365d7f6bad4b336f10aa674b52b1b
Haroldo Cruz <msom.info@gmail.com>
2020-09-13 12:49:58 -0300

    refactor(layout): profile; others
    Inject profile services for dropdown options get list of profile names.
    Organize menu items; inserting a horizontal bar to separe distinct items.

--- src/app/layout/nav/nav-perfil/nav-perfil.component.ts
+++ src/app/layout/nav/nav-perfil/nav-perfil.component.ts
@@ -1,3 +1,4 @@
+import { ProfileService } from './../../../services/profile.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavPerfilComponent implements OnInit {
 
-  constructor() { }
+  constructor(public profileService: ProfileService) { }
 
   ngOnInit(): void {
   }
