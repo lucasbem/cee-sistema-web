a00c33adbb8352d5a0b90fcf5185d7977a6060f7
Haroldo Cruz <msom.info@gmail.com>
2020-09-15 02:53:30 -0300

    refactor(user): filter, debounce;
    Inserting debounce effect to improve search performance.

--- src/app/features/user/user.component.ts
+++ src/app/features/user/user.component.ts
@@ -1,6 +1,8 @@
 import { UserService } from './user.service';
 import { IUser, User } from './../../interfaces/User';
 import { Component, OnInit } from '@angular/core';
+import { Observable, fromEvent } from "rxjs";
+import { debounceTime, distinctUntilChanged, map, filter, switchMap } from "rxjs/operators";
 
 @Component({
   selector: 'app-user',
@@ -9,14 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  userList: IUser[]
-  // filtro: string;
+  // public filterInputId = new FormControl();
+  public filtro: string;
 
-  constructor(public userService: UserService) {}
+  constructor(public userService: UserService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
 
-  edit(user: IUser | null){
+
+    this.typeahead(document.getElementById('search-box')).subscribe(data => {
+      this.userService.filtro = data;
+    });
+  }
+
+  typeahead(elem: Element): Observable<any>{
+    // const searchBox = ;
+
+    return fromEvent(elem, 'input').pipe(
+      map((e: KeyboardEvent) => (e.target as HTMLInputElement).value),
+      // filter(text => text.length > 2),
+      debounceTime(600),
+      distinctUntilChanged()
+      // switchMap(() => ajax('/api/endpoint'))
+    );
+  }
+
+  edit(user: IUser | null) {
     this.userService.user = (user) ? user : new User();
   }
 
