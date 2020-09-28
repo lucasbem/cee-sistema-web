c2e503492d313891618754112b3a176ffa27c6e4
Haroldo Cruz <msom.info@gmail.com>
2020-09-28 12:25:54 -0300

    feature(institution): create components
    Create components: institution, course, maintained, maintainer

---
+++ src/app/src/app/features/institution/maintained/maintained.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maintained',
+  templateUrl: './maintained.component.html',
+  styleUrls: ['./maintained.component.less']
+})
+export class MaintainedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/src/app/features/institution/maintainer/maintainer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maintainer',
+  templateUrl: './maintainer.component.html',
+  styleUrls: ['./maintainer.component.less']
+})
+export class MaintainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
