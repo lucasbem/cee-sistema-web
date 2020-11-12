acf9979aaf32ef67189c4c250a95d1e0816e006f
Haroldo Cruz <msom.info@gmail.com>
2020-11-12 18:21:21 -0300

    refactor(institution): maintainer

--- src/app/features/institution/maintainer/maintainer.component.ts
+++ src/app/features/institution/maintainer/maintainer.component.ts
@@ -1,3 +1,5 @@
+import { IMaintainer } from './maintainer.model';
+import { MaintainerService } from './maintainer.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaintainerComponent implements OnInit {
 
-  constructor() { }
+  // maintainer = {
+  //   "id": "2",
+  //   "name": "Mantida2",
+  //   "legalAct": [
+  //     {
+  //       "name": "Doc1",
+  //       "title": "Doc1",
+  //       "description": "Doc1",
+  //       "link": "",
+  //       "type": ""
+  //     }
+  //   ],
+  //   "socialReason": "Mantida2",
+  //   "other": "Mantida2",
+  //   "contact": {
+  //     "email": [
+  //       "email@mantida.com"
+  //     ],
+  //     "phone": [
+  //       {
+  //         "number": 5563987654321,
+  //         "type": "celular"
+  //       }
+  //     ],
+  //     "address": [
+  //       {
+  //         "country": "Brasil",
+  //         "place": "Rua 26 quadra 119"
+  //       }
+  //     ]
+  //   }
+  // }
+  maintainer: IMaintainer;
+  MaintainerService = MaintainerService;
+
+  constructor(
+    public maintainerService: MaintainerService
+  ) { }
 
   ngOnInit(): void {
+    this.maintainer = MaintainerService.maintainer;
   }
 
 }
